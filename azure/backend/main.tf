3796d8ea64e855bb58cb4c8f7e765061d2ba5e74
Hozefa Ezzy <hozefaezzy@hotmail.com>
2025-10-19 22:44:08 +0530

    Add files via upload

--- azure/backend/main.tf
+++ azure/backend/main.tf
@@ -1,11 +1,15 @@
-provider "azurerm" {
-  version = "~> 1.42"
-  client_id       = var.client_id   # ENVIRONMENT VARIABLE
-  client_secret   = var.client_secret # ENVIRONMENT VARIABLE
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-}
-
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+      client_id       = var.client_id   # ENVIRONMENT VARIABLE
+      client_secret   = var.client_secret # ENVIRONMENT VARIABLE
+      subscription_id = var.subscription_id
+      tenant_id       = var.tenant_id
+    }
+  }
+  
 resource "azurerm_resource_group" "resource_group" {
   name     = "${var.resource_group}_${var.environment}"
   location = var.location
