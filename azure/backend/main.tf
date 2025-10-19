3f0e699b7842dc8ce0ee21f5eb6227eaac2e2ce8
Hozefa Ezzy <hozefaezzy@hotmail.com>
2025-10-19 22:52:35 +0530

    Update main.tf

--- azure/backend/main.tf
+++ azure/backend/main.tf
@@ -8,8 +8,7 @@ terraform {
 }
 
 provider "azurerm" {
-  # features = {}
-
+  features = {}
   client_id       = var.client_id
   client_secret   = var.client_secret
   subscription_id = var.subscription_id
